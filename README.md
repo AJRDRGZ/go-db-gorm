15654147958714c3e2bfdb9939a9e189a406fb0d
Alejandro Rodriguez <aj.softwaredeveloper@gmail.com>
2020-06-25 00:51:35 -0500

    Clase 4.5 CREATE

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ storage.DB().AutoMigrate(
     &model.InvoiceItem{},
 )
 ```
+
+## Crear Productos
+
+```go
+product1 := model.Product{
+    Name:  "Curso de Go",
+    Price: 120,
+}
+
+obs := "testing with Go"
+product2 := model.Product{
+    Name:         "Curso de Testing",
+    Price:        150,
+    Observations: &obs,
+}
+
+product3 := model.Product{
+    Name:  "Curso de Python",
+    Price: 200,
+}
+
+storage.DB().Create(&product1)
+storage.DB().Create(&product2)
+storage.DB().Create(&product3)
+```
--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func main() {
-	driver := storage.Postgres
+	driver := storage.MySQL
 	storage.New(driver)
 
-	storage.DB().AutoMigrate(
-		&model.Product{},
-		&model.InvoiceHeader{},
-		&model.InvoiceItem{},
-	)
+	product1 := model.Product{
+		Name:  "Curso de Go",
+		Price: 120,
+	}
+
+	obs := "testing with Go"
+	product2 := model.Product{
+		Name:         "Curso de Testing",
+		Price:        150,
+		Observations: &obs,
+	}
+
+	product3 := model.Product{
+		Name:  "Curso de Python",
+		Price: 200,
+	}
+
+	storage.DB().Create(&product1)
+	storage.DB().Create(&product2)
+	storage.DB().Create(&product3)
 }
